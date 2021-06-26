aeb9ab94baa23feb7caf58b30e58a67b87501e04
ranjeet singh <ranjeetsingh@ranjeets-MacBook-Pro.local>
2021-06-27 00:01:54 +0530

    added header

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','*');
+    res.header('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
     next();
 });
